e9745d939c1cfc4556cde05d8b77b8d5a07b74f2
Aula03 <Aula03@AULAIMACE21.local>
2024-10-29 12:20:21 +0100

    Sesion05.2

---
+++ GestionMascotas/GestionMascotas/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  GestionMascotas
+//
+//  Created by Aula03 on 29/10/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ GestionMascotas/GestionMascotasTests/GestionMascotasTests.swift
@@ -0,0 +1,17 @@
+//
+//  GestionMascotasTests.swift
+//  GestionMascotasTests
+//
+//  Created by Aula03 on 29/10/24.
+//
+
+import Testing
+@testable import GestionMascotas
+
+struct GestionMascotasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
